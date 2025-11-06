05ce8f6828a2b86b654bd1c085aa5f556ef960e2
김해찬 <gibbm1127@naver.com>
2025-11-06 18:47:57 +0900

    init

---
+++ src/main/java/site/haechan/sns_backend/SnsBackendApplication.java
@@ -0,0 +1,13 @@
+package site.haechan.sns_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/haechan/sns_backend/SnsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.haechan.sns_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
