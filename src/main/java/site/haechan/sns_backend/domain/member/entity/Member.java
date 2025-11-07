c8ec030598c69de171866867226d2236f3f8bf11
김해찬 <gibbm1127@naver.com>
2025-11-07 22:02:58 +0900

    refactor: BaseEntity를 활용하여 BoilerPlate를 제거

--- src/main/java/site/haechan/sns_backend/domain/member/entity/Member.java
+++ src/main/java/site/haechan/sns_backend/domain/member/entity/Member.java
@@ -1,42 +1,14 @@
 package site.haechan.sns_backend.domain.member.entity;
 
-import java.time.Instant;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
+import site.haechan.sns_backend.global.common.entity.BaseEntity;
 
-@Getter
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-@Builder(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Member {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-
-	@CreatedDate
-	@Column(updatable = false, nullable = false)
-	private Instant createdAt;
-
-	@LastModifiedDate
-	@Column(nullable = false)
-	private Instant updatedAt;
-
+public class Member extends BaseEntity {
 	@Column(nullable = false, unique = true, length = 50)
 	private String username;
 
@@ -61,7 +33,6 @@ public class Member {
 	@Column(length = 255, nullable = false)
 	private Long followingCount;
 
-	@Builder.Default
 	@Column(nullable = false, columnDefinition = "TINYINT(1) default 0")
 	private Boolean isSecret = false;
 }
